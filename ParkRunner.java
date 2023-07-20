4784231f291eff9dcd611305dfd9daa5010ef53a
pallavi <pallavisxworkz@gmail.com>
2023-07-20 21:59:28 +0530

    chaining

---
+++ ParkRunner.java
@@ -0,0 +1,16 @@
+class ParkRunner{
+	public static void main(String[] args){
+		
+		Park park = new park("Freedom","normal","Banglore");
+		
+		
+		System.out.println("##################################");
+		
+		Park park = new park("Gandi","Student","Shimogga",15);
+		
+		System.out.println("##################################");
+		
+		Park park = new park("vinayaka","Children","shimogga",13,"Pallavi")
+		
+	}
+}
