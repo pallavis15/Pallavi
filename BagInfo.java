d4e3e0c46d97e4ef10ab6b05f56dda7a1224c48c
pallavi <pallavisxworkz@gmail.com>
2023-07-20 12:02:53 +0530

    array related

---
+++ BagInfo.java
@@ -0,0 +1,21 @@
+class BagInfo{
+    
+	public static void main(String[] args)
+	{
+	System.out.println("Bag information");
+	String color = "white";
+	float price = 2999.0f;
+    String brand = "puma";
+	String type = "Handbag";
+	char size = 'M';
+	
+	System.out.println("Color:" +color);	
+	System.out.println("Price:" +price);
+	System.out.println("Brand:" +brand);
+	System.out.println("Type:" +type);
+	System.out.println("Size:" +size);
+	}
+	
+	
+}
+
