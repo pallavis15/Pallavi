ecb24170b4216f53fc42ba82acd1701216d55965
pallavis15 <140036434+pallavis15@users.noreply.github.com>
2023-08-30 08:01:31 +0530

    Add files via upload

---
+++ Pant.java
@@ -0,0 +1,11 @@
+package com.xworkz.Farmer.app;
+
+public class Pant {
+	System.out.println("invoking main in PantRunner");
+	int size=28;
+	Zip zip=new Zip();
+	Pant pan=new Pant(size);
+	pan.setPant(zip);
+	pan.washing();
+
+}
